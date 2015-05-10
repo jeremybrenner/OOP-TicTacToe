658540a141033a4b5230e3bd070a257970d27d63
jmbrenne <jeremybrenner0@gmail.com>
2015-05-09 19:17:43 -0700

    Updated game with two JS variations

--- app.js
+++ app.js
@@ -2,41 +2,49 @@
 
 // Execute this code only AFTER the document is ready
 // Hint: use jQuery's `$(document).ready`
-
   function Game() {
-    //Create a new instance of player 1
-    //this.player1 = ...
-
-    //Do the same for a player 2
-    //this.player2 = ...
-    
-    //Create the board
-    //this.board = ...
+  
+      this.turns = 0
+      this.player1 = new Player('X');
+      this.player2 = new Player('O');  
   }
-
   // Remember: prototypes are shared functions between all game instances
-  Game.prototype.nextPlayer = function() {
-    //Switch players
+  Game.prototype.nextPlayer = function(currentCell) {
+    console.log("turns works")
+    this.turns ++;
+    console.log(currentCell);
+    if(this.turns%2 !== 0){
+      console.log("player 1")
+      currentCell.css("background-color", "blue");
+    } else {
+      console.log("player 2")
+      currentCell.css("background-color", "red");
+    }
   };
 
   // `Game.prototype.init` kicks off a new game with a board and two players
   Game.prototype.init = function() {
-    //
+      // this.$cells = $('.box')
+      console.log("this worked")
+      this.board = new Board();
   };
 
   // A starter Player constructor.
   function Player(team) {
-    //Is the player X or O?
-    //this.team = ...
+    this.team = team;
   };
 
   // A starter Board constructor.
   function Board() {
-    //Tracks the cells of the board instance
-    //this.$cells = ...
-
+    this.$cells = $('.box')   
     //Store any other properties that board may have below, such as a reset option
   };
+
+   Board.prototype.makeMove = function(){
+    this.$cells.click(function(){
+        game.nextPlayer(this.$cells);
+      }
+    });
 
   // Start the game!
   var game = new Game();
