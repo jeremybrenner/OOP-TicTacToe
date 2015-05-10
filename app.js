0f0fd70cfe0e1877c17ceb176e47e631417fb79b
jmbrenne <jeremybrenner0@gmail.com>
2015-05-10 00:16:31 -0700

    Added reset button logic and comments

--- app.js
+++ app.js
@@ -1,49 +1,56 @@
-// OOP Tic Tac Toe boilerplate code
 
-// Execute this code only AFTER the document is ready
-// Hint: use jQuery's `$(document).ready`
-
+  // Creates a new game instance, new board, and new players
   function Game() {
 
       this.playerX = new Player('X');
-      this.playerO = new Player('O'); 
+      this.playerO = new Player('O');
       this.board = new Board();
   }
 
-  // Remember: prototypes are shared functions between all game instances
+  // This sets the turn without relying on an incrementing counter
   Game.prototype.nextPlayer = function() {
-  game.currentPlayer === game.playerX ? game.currentPlayer = game.playerO : game.currentPlayer = game.playerX;
+      game.currentPlayer === game.playerX ? game.currentPlayer = game.playerO : game.currentPlayer = game.playerX;
   };
 
-  // Game.prototype.checkWin = function() {
-  // this will go in the nextPlayer function
-  // }
 
   // `Game.prototype.init` kicks off a new game with a board and two players
   Game.prototype.init = function() {
-    this.currentPlayer = this.playerX
-    $(this.board.$cells).click(function(event){
-      $(event.target).text(game.currentPlayer.team);
-      game.nextPlayer();
+    this.currentPlayer = this.playerX;
+    $(game.board.$cells).click(function(event){
+      game.board.makeMove();
     });
   };
 
-  // A starter Player constructor.
+  // Constructs a player object and assigns a team
   function Player(team) {
     this.team = team;
-  };
-
-  // A starter Board constructor.
-  function Board() {
-    //Tracks the cells of the board instance
-    //this.$cells = ...
-    this.$cells = $('.box') 
-    //Store any other properties that board may have below, such as a reset option
-  };
-
-  Board.prototype.makeMove = function(player, $cell) {
-    $cell.text(player);
   }
+
+  // Creates a new board object on game creation
+  function Board() {   
+    this.$cells = $('.box');
+     $('#reset').click(function(event){
+      game.board.resetBoard();
+    });
+   }
+
+  // Sets a square to player 'team' and prevents square 
+  // from reset with another move
+  Board.prototype.makeMove = function() {
+    if($(event.target).html() === '&nbsp;'){
+      $(event.target).text(game.currentPlayer.team);
+        game.nextPlayer();
+          } else { 
+            alert("This square is already taken!");
+          }
+  };
+
+  // Generic function to reset the board to its initial state,
+  // will be called by the button as a hard reset, and by the
+  // winner check 
+  Board.prototype.resetBoard = function() {
+     game.board.$cells.html('&nbsp;');
+  };
 
   // Start the game!
   var game = new Game();
